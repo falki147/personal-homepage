b68458956f4bcfe854139d44e66e86e64e5d8072
Florian Preinfalk <preinfalkflorian@gmail.com>
2023-03-04 17:33:11 +0100

    Make navbar responsive

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,34 @@ function checkScrolled() {
   }
 }
 
+function ready(cb) {
+  if (document.readyState === 'complete') {
+    cb();
+  }
+  else {
+    document.addEventListener('DOMContentLoaded', cb);
+  }
+}
+
+ready(() => {
+  for (const button of document.querySelectorAll('.navbar-button')) {
+    button.addEventListener('click', () => {
+      button.classList.toggle('open');
+
+      const controls = button.getAttribute('aria-controls');
+      if (!controls) {
+        return;
+      }
+
+      const controlledElement = document.getElementById(controls);
+      if (!controlledElement) {
+        return;
+      }
+
+      controlledElement.classList.toggle('open', button.classList.contains('open'));
+    });
+  }
+});
+
 document.addEventListener('scroll', checkScrolled);
 checkScrolled();
